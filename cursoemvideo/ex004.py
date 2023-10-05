8d0c82fec2dab12760337907461298700e525218
Arthur Campos <142673523+arthurcamposm@users.noreply.github.com>
2023-10-05 00:09:46 -0300

    Update ex004.py

--- cursoemvideo/ex004.py
+++ cursoemvideo/ex004.py
@@ -1,3 +1,4 @@
+# Dissecando uma Variável
 x = input('Digite algo:')
 print(type(x), f'Possui números? {x.isnumeric()} Possui letras? {x.isalpha()} Possui letra ou número? {x.isalnum()}')
 print(f'Possui letras minúsculas? {x.islower()} Possui letras maiúsculas? {x.isupper()}')
